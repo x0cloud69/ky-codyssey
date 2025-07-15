c67f45fca1a105aaec451d404852075b44a09d29
KY <x0cloud69@gmail.com>
2025-07-15 11:03:23 +0900

    2번째 과젱

---
+++ ky-codyssey-main/Codyseey/app.py
@@ -0,0 +1,26 @@
+""" ✅ 1. 웹 환경 테스트 (app.py)
+Flask를 사용하여 간단한 메시지를 출력하는 웹 애플리케이션을 구현하고, VS Code 환경에서 실행한다.
+
+Python 파일명: app.py
+구현 요구사항:
+Flask 웹 프레임워크 사용
+@app.route("/") 데코레이터 사용
+함수 이름: hello_world
+반환 문자열: "Hello, DevOps!"
+서버 실행 시 호스트: 0.0.0.0, 포트: 8080
+Visual Studio Code의 Run 메뉴에서 Run Without Debugging를 선택해서 해당 파일을 실행한다.
+웹브라우저로 접속하여 Hello, DevOps! 출력을 확인한다.
+Visual Studio Code의 Run 메뉴에서 Stop Debugging를 선택해서 실행을 종료한다.
+Visual Studio Code의 Run 메뉴에서 Start Debugging과 Run Without Debugging을 각각 실행해보고 차이점을 확인한다.
+ """
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+  return "Hello DevOps~"
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=8080)
---
+++ ky-codyssey-main/Codyseey/power_calculator.py
@@ -0,0 +1,66 @@
+"""  2. 제곱 계산 프로그램 구현하기 - power_calculator.py
+Python을 사용하여 제곱 계산 프로그램을 구현한다.
+
+Python 파일명: power_calculator.py
+
+구현 요구사항:
+
+사용자로부터 숫자와 제곱할 횟수(지수)를 입력받음
+입력받은 숫자를 지정한 횟수만큼 거듭제곱한 결과 출력
+Python의 내장 연산자 ** 또는 pow() 함수 사용 금지
+반복문을 사용하여 직접 제곱 계산 구현
+숫자는 float(), 지수는 int()로 형변환
+사용자 입력 예시:
+
+Enter number: 3
+Enter exponent: 4
+출력 형식:
+
+Result: 81
+예외 처리:
+
+숫자 입력이 숫자형이 아닐 경우 "Invalid number input." 출력
+지수 입력이 정수가 아닐 경우 "Invalid exponent input." 출력
+테스트 방식:
+
+Visual Studio Code에서 power_calculator.py 파일을 열고 터미널에서 python power_calculator.py 명령어로 실행한다.
+프로그램의 실행 로직: """
+
+def calculate_power(base, exponent):
+  #거급제곱 계산을 위한 함수
+  result = 1
+  
+  if exponent >= 0:
+    for _ in range(exponent):
+      result = result * base
+  else:
+    for _ in range(-exponent):
+      result = result * base
+    result = 1 / result
+  
+  return result
+
+def main():
+  #숫자(base)와 지수(exponent) 입력 및 예외처리
+  try:
+    base = float(input("Enter number: "))
+  except ValueError:
+    print("Invalid number input.")
+    return
+
+  #지수 (exponent) 입력 및 예외처리
+  try:
+    exponent = int(input("Enter exponent: "))
+  except ValueError:
+    print("Invalid exponent input.")
+    return
+
+  # 결과 계산 및 출력
+  result = calculate_power(base,exponent)
+  print(f"Result: {result}")
+
+if __name__ == "__main__":
+  main()
+  
+
+
